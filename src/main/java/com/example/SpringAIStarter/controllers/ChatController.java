070a0c2307846508b0864646f951cfc2c9f1525b
singhsanket143 <singhsanket143@gmail.com>
2026-04-21 18:25:17 +0100

    Added structured prompt response

--- src/main/java/com/example/SpringAIStarter/controllers/ChatController.java
+++ src/main/java/com/example/SpringAIStarter/controllers/ChatController.java
@@ -1,10 +1,15 @@
 package com.example.SpringAIStarter.controllers;
 
+import java.util.List;
+
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dtos.MovieRecommendation;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,6 +31,18 @@ public class ChatController {
             .call()
             .content();
         return response;
+    }
+
+    @GetMapping("/movies")
+    public List<MovieRecommendation> getMovieRecommendations(@RequestParam String genre, @RequestParam int count) {
+        return chatClient.prompt()
+        .user(u -> u.text("""
+                Recommend exactly {count} movies in the {genre} genre. For each movie, provide the accurate title, description, genre, rating, year, and actors.
+                The rating should be a number between 0 and 10, and the year should be the year of the movie released. The data should be absolutely accurate.
+                Everytime even the genre is same, the movies should be different.
+                """).param("count", String.valueOf(count)).param("genre", genre))
+        .call()
+        .entity(new ParameterizedTypeReference<List<MovieRecommendation>>() {});
     }
     
 }
---
+++ src/main/java/com/example/dtos/MovieRecommendation.java
@@ -0,0 +1,23 @@
+package com.example.dtos;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MovieRecommendation {
+
+    String title;
+    String description;
+    String genre;
+    String rating;
+    String year;
+    List<String> actors;
+    
+}
